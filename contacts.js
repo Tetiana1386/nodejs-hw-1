30093486401788b06e8247ecae3a9f88798602f9
Tetiana1386 <tanyashostak13@gmail.com>
2021-09-18 19:03:35 +0300

    added add_contact

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
+const shortid = require('shortid');
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
@@ -37,6 +38,23 @@ function removeContact(contactId) {
     } catch (error) {
         console.log(error.message);
     }
+
 };
 
-module.exports = { listContacts, getContactById, removeContact };
+function addContact(name, email, phone) {
+    try {
+        fs.readFile(contactsPath, 'utf8')
+            .then((data) => {
+                const users = JSON.parse(data);
+                const newUser = { id: shortid.generate(), name, email, phone };
+                const newUsers = [...users, newUser];
+                fs.writeFile(contactsPath, JSON.stringify(newUsers));
+                console.log("Contact was successfully added");
+                console.table(newUsers);
+            });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
