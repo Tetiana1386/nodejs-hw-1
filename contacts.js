5d734bf03d8de4977bb5cf1fb940ec659383f427
Tetiana1386 <tanyashostak13@gmail.com>
2021-09-18 22:50:02 +0300

    adds conditions

--- contacts.js
+++ contacts.js
@@ -1,60 +1,86 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 const shortid = require('shortid');
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 function listContacts() {
-    try {
-        fs.readFile(contactsPath, 'utf8')
-        .then((data) => console.table(JSON.parse(data)));
-        } catch(error) {
-        console.log(error.message);
-    }
+    fs.readFile(contactsPath, 'utf8', (error, data) => {
+        if (error) throw error;
+
+        console.table(JSON.parse(data));
+    });
 };
 
 function getContactById(contactId) {
-    try {
-        fs.readFile(contactsPath)
-            .then((data) => {
-                const users = JSON.parse(data);
-                const user = users.find(user => user.id == contactId);
-                console.table(user);
-            })
-    } catch (error) {
-        console.log(error.message);
-    };
+    fs.readFile(contactsPath, 'utf8', (error, data) => {
+        if (error) throw error;
+
+        const users = JSON.parse(data);
+        const user = users.find(user => user.id == contactId);
+        console.table(user);
+    });
 };
 
 function removeContact(contactId) {
-    try {
-        fs.readFile(contactsPath)
-            .then((data) => {
-                const users = JSON.parse(data);
-                const newUsers = users.filter(user => user.id != contactId);
-                fs.writeFile(contactsPath, JSON.stringify(newUsers, null, '\t'));
-                console.log('Contact removed');
-        })
-    } catch (error) {
-        console.log(error.message);
-    }
+    fs.readFile(contactsPath, 'utf8', (error, data) => {
+        if (error) throw error;
 
+        const users = JSON.parse(data);
+        const newUsers = users.filter((user) => user.id !== contactId);
+
+        if (newUsers.length === users.length) {
+            console.log(
+                `Contact with such id "${contactId}" not found!`
+            );
+            return;
+        };
+
+        fs.writeFile(contactsPath, JSON.stringify(newUsers), (error) => {
+            if (error) throw error;
+        });
+
+        console.log(
+            `Contact with such id "${contactId}" deleted! New list of contacts: `
+        );
+        console.table(newUsers);
+    });
 };
 
 function addContact(name, email, phone) {
-    try {
-        fs.readFile(contactsPath, 'utf8')
-            .then((data) => {
-                const users = JSON.parse(data);
-                const newUser = { id: shortid.generate(), name, email, phone };
-                const newUsers = [...users, newUser];
-                fs.writeFile(contactsPath, JSON.stringify(newUsers));
-                console.log("Contact was successfully added");
-                console.table(newUsers);
-            });
-    } catch (error) {
-        console.log(error.message);
-    }
+    fs.readFile(contactsPath, 'utf8', (error, data) => {
+        if (error) throw error;
+
+        const users = JSON.parse(data);
+
+        if (
+            users.find(user => user.name.toLowerCase() === name.toLowerCase(),
+            )
+        )
+            return console.warn('This name already exists!');
+        
+        if (
+            users.find(user => user.email === email,
+            )
+        )
+            return console.warn('This email already exists!');
+        
+        if (
+            users.find(user => user.phone === phone,
+            )
+        )
+            return console.warn('This phone already exists!');
+        
+        const newUser = { id: shortid.generate(), name, email, phone };
+        const newUsers = [...users, newUser];
+
+        fs.writeFile(contactsPath, JSON.stringify(newUsers), (error) => {
+            if (error) throw error;
+        });
+
+        console.log("Contact was added successfully");
+        console.table(newUsers);
+    });
 };
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
